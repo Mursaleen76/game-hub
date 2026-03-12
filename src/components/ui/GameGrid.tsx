592754cfd085a00d53128da954f328e43ea9d3d2
Mursaleen76 <mursaleensohail76@gmail.com>
2026-03-12 17:57:18 +0500

    Refactor GameGrid

--- src/components/ui/GameGrid.tsx
+++ src/components/ui/GameGrid.tsx
@@ -14,27 +14,23 @@ export interface Props {
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
+
+  if (error) return <Text>{error}</Text>;
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 3, xl: 4 }} spacing={5} padding="20px">
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton></GameCardSkeleton>
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game}></GameCard>
+    <SimpleGrid columns={{ sm: 1, md: 3, xl: 4 }} spacing={5} padding="20px">
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton></GameCardSkeleton>
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game}></GameCard>
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
 export default GameGrid;
-// function useGame() {
-//   throw new Error("Function not implemented.");
-// }
